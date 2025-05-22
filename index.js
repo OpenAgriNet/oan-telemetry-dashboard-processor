fa93f58aab090ce9cc8eb94dc266c87fd00da2d0
shashank-kenpath <104070563+shashank-kenpath@users.noreply.github.com>
2025-05-22 14:08:26 +0530

    DB changes for prod

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT || "5432", 10),
-  ssl: {
-    rejectUnauthorized: true,
-    ca: fs.readFileSync(path.join(__dirname, 'certs', 'rds-global.pem')).toString()
-  }
+  // ssl: {
+  //   rejectUnauthorized: true,
+  //   ca: fs.readFileSync(path.join(__dirname, 'certs', 'rds-global.pem')).toString()
+  // }
 });
 
 
